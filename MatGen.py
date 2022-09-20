2e01c5cb6b1a6b75f46e0d0162ee7b8e4822a6ac
Jordan.mershimer@gmail.com <ie00YnfYWZ6UQwE96KUb>
2022-09-20 12:53:50 -0400

    MatGen MBene

---
+++ MatGen.py
@@ -0,0 +1,44 @@
+import periodictable
+
+pt = periodictable
+M_MatN = 0
+X_MatN = 0
+M_mat = ([["Ti"],
+          ["Hf"],
+          ["V"],
+          ["Nb"],
+          ["Ta"],
+          ["Cr"],
+          ["Mo"],
+          ["W"],
+          ["Mn"],
+          ["Tc"],
+          ["Sc"],
+          ["Zr"],
+          ["Ru"],
+          ["Fe"],
+          ["Ni"],
+          ["Rh"]])
+X_Mat = ([["B"],
+          ["N"],
+          ["C"]])
+for M_MatN2 in M_mat:
+    for X_MatN2 in X_Mat:
+        print(M_MatN2, X_MatN2, "2")
+        continue
+for M_MatN2 in M_mat:
+    for X_MatN2 in X_Mat:
+        print(M_MatN2,"3", X_MatN2, "4")
+        continue
+for M_MatN2 in M_mat:
+    for X_MatN2 in X_Mat:
+        print(M_MatN2, X_MatN2)
+        continue
+for M_MatN2 in M_mat:
+    for X_MatN2 in X_Mat:
+        print(M_MatN2, "2", X_MatN2)
+        continue
+for M_MatN2 in M_mat:
+    for X_MatN2 in X_Mat:
+        print(M_MatN2,"5", X_MatN2, "2")
+        continue
